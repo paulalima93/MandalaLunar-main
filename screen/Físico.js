89db845832d877b411dac7e0ef453f34f810b94a
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2023-09-26 16:45:01 -0300

    Update Físico.js

--- screen/Físico.js
+++ screen/Físico.js
@@ -1,29 +1,84 @@
-import React, {Component} from 'react'
-import {Text, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet, Image, FlatList } from 'react-native';
 import Ionicons from "react-native-vector-icons/Ionicons";
 import * as Font from 'expo-font'
-var fonte ={
-    'montserrat':require('../assets/fonte.ttf')
+var fonte = {
+    'montserrat': require('../assets/fonte.ttf')
 }
 
+const dados = [
+    {
+        id: "01",
+        title: 'Ovulação',
+        icon: "stop-circle-outline",
+    },
+    {
+        id: "02",
+        title: 'Dor de \n Cabeça',
+        icon: "medical-outline",
+    },
+    {
+        id: "03",
+        title: 'Espinhas',
+        icon: "bandage-outline",
+    },
+    {
+        id: "04",
+        title: 'Inchaço',
+        icon: "aperture-outline",
+    },
+    {
+        id: "05",
+        title: 'Cólica',
+        icon: "scan-outline",
+    },
+    {
+        id: "06",
+        title: 'Dor nos seios',
+        icon: "recording-outline",
+    },
+]
 export default class Físico extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          fontsLoaded: false,
-          
-        };}
+            fontsLoaded: false,
+            ovulacao: false,
+            dor_cabeca: false,
+            espinhas: false,
+            inchaco: false,
+            colica: false,
+            dor_seios:false
+
+        };
+    }
+
+    renderItem = ({ item }) => {
+        return(
+            <View style={{flexDirection: 'row', alignItems: 'center', width: "49%"}}>
+                <TouchableOpacity style={styles.button}>
+                        <Ionicons
+                            name={item.icon}
+                            size={20}
+                            color={'black'}
+                        />
+                    </TouchableOpacity>
+                    <Text style={styles.text}>{item.title}</Text>
+            </View>
+        )
+
+    }
 
     async _loadFontsAsync() {
         await Font.loadAsync(fonte);
         this.setState({ fontsLoaded: true });
-      }
+    }
 
-      componentDidMount() {
+    componentDidMount() {
         this._loadFontsAsync();
-      }
-    render (){
-        return(
+    }
+    render() {
+        return (
 
             <View style={styles.container}>
                 <View style={styles.styleTitle}>
@@ -33,75 +88,24 @@ export default class Físico extends Component {
                 </View>
 
                 <Text style={styles.textText}>Se você quiser, pode escolher um ícone para registrar as questões físicas do seu corpo no dia de hoje</Text>
-                
-                <View style={styles.containerButton}>    
-                    <TouchableOpacity style={styles.button}>
-                    <Ionicons
-                            name={'stop-circle-outline'}
-                            size={20}
-                            color={'black'}
-                        />
-                    </TouchableOpacity>
-                    <Text style={styles.text}>Ovulação</Text>
-                    <TouchableOpacity style={styles.button}>
-                    <Ionicons
-                            name={'aperture-outline'}
-                            size={20}
-                            color={'black'}
-                        />
-                    </TouchableOpacity>
-                    <Text style={styles.text}>Inchaço</Text>
-                </View>
-                <View style={styles.containerButton}>    
-                    <TouchableOpacity style={styles.button}>
-                    <Ionicons
-                            name={'medical-outline'}
-                            size={20}
-                            color={'black'}
-                        />
-                    </TouchableOpacity>
-                    <Text style={styles.text}>Dor de cabeça</Text>
-                    <TouchableOpacity style={styles.button}>
-                        <Ionicons
-                            name={'scan-outline'}
-                            size={20}
-                            color={'black'}
-                        />
-                        </TouchableOpacity>
-                    <Text style={styles.text}>Cólica</Text>
-                </View>
-                <View style={styles.containerButton}>
-                    <TouchableOpacity style={styles.button}>
-                            <Ionicons
-                                name={'bandage-outline'}
-                                size={20}
-                                color={'black'}
-                            />
-                            </TouchableOpacity>
-                        <Text style={styles.text}>Espinhas</Text>
-                        <TouchableOpacity style={styles.button}>
-                            <Ionicons
-                                name={'recording-outline'}
-                                size={20}
-                                color={'black'}
-                            />
-                            </TouchableOpacity>
-                        <Text style={styles.text}>Dor nos seios</Text>
-                </View>
 
-               
-            
+                <FlatList 
+                    data={dados}
+                    renderItem={this.renderItem}
+                    keyExtractor={item => item.id}
+                    numColumns = {2}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    image:{
+    image: {
         height: 20,
         width: 20
     },
-    button:{
+    button: {
         backgroundColor: '#96b5a6',
         borderRadius: 25.5,
         height: 51,
@@ -110,31 +114,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 15
     },
-    iconButton:{
-        position: 'absolute',
-        backgroundColor: '#FEBEAC',
-        borderRadius: 25,
-        height: 77,
-        width: 84,
-        marginTop: 6,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    containerButton:{
+    containerButton: {
         flexDirection: 'row',
         marginTop: 12,
-        alignItems: 'center',
-
+        alignItems: "center",
+        justifyContent: 'space-between',
     },
-    styleTitle:{
+    styleTitle: {
         flexDirection: 'row'
     },
-    Titletext:{
+    Titletext: {
         fontFamily: 'montserrat',
         fontSize: 40,
         marginTop: 20,
     },
-    textText:{
+    textText: {
         marginTop: 20,
         fontFamily: 'montserrat',
         fontSize: 19,
@@ -142,12 +136,12 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10
     },
-    container:{
+    container: {
         alignItems: 'center',
         backgroundColor: '#fce1cb',
         flex: 1
     },
-    text:{
+    text: {
         fontFamily: 'montserrat',
         fontSize: 21,
         marginTop: 9,
